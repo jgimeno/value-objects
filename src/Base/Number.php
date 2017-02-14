cba1cc4fcaa226a582044b4d184bff31afccfdc7
Jonathan Gimeno <jgimeno@gmail.com>
2017-02-14 22:51:54 +0100

    Create a Number class.

---
+++ src/Base/Number.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ValueObjects\Base;
+
+use ValueObjects\ValueObject;
+
+/**
+ * Class Number
+ * Value object that represents a number. It can perform basic artihmetical operations.
+ * @package ValueObjects\Base
+ */
+class Number extends ValueObject
+{
+    public function __construct($value)
+    {
+        $this->validate($value);
+
+        parent::__construct($value);
+    }
+
+    /**
+     * Validates that the number is numeric.
+     * @param $value
+     */
+    protected function validate($value)
+    {
+        if (!is_numeric($value) || is_bool($value)) {
+            throw new \InvalidArgumentException("Can't create a Number from a non numeric value.");
+        }
+    }
+}
---
+++ tests/Base/NumberTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace ValueObjects\Base;
+
+use ValueObjects\ValueObject;
+
+class NumberTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider nonNumericValues
+     */
+    public function testItThrowsExceptionWhenCreatingItFromNoNumericValue($value)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        new Number($value);
+    }
+
+    public function nonNumericValues()
+    {
+        $set = [];
+
+        $set['an array'] = [
+            'value' => []
+        ];
+
+        $set['an string'] = [
+            'value' => 'a string'
+        ];
+
+        $set['an object'] = [
+            'value' => new ValueObject(1)
+        ];
+
+        $set['a boolean'] = [
+            'value' => true
+        ];
+
+        return $set;
+    }
+
+    /**
+     * @dataProvider numericValuesAndStringConversions
+     */
+    public function testWeCanConvertANumberToString($number, string $numberAsString)
+    {
+        $number = new Number($number);
+
+        $this->assertEquals($numberAsString, $number);
+    }
+
+    public function numericValuesAndStringConversions()
+    {
+        $set = [];
+
+        $set['integer'] = [
+            'number' => 123,
+            'numberAsString' => '123'
+        ];
+
+        $set['float'] = [
+            'number' => 123.2,
+            'numberAsString' => '123.2'
+        ];
+
+        $set['hexadecimal'] = [
+            'number' => 0x539,
+            'numberAsString' => '1337'
+        ];
+
+        $set['binary'] = [
+            'number' => 0b10100111001,
+            'numberAsString' => '1337'
+        ];
+
+        $set['exponential'] = [
+            'number' => 1337e0,
+            'numberAsString' => '1337'
+        ];
+
+        return $set;
+    }
+}
