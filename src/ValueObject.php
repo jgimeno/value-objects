263dd55920d1fc1f21d1d5856e0d93262ccb8c4b
Jonathan Gimeno <jgimeno@gmail.com>
2017-02-23 22:03:09 +0100

    Fixes in format.

--- src/ValueObject.php
+++ src/ValueObject.php
@@ -16,8 +16,15 @@ abstract class ValueObject implements ValueObjectInterface
     public function equals(ValueObjectInterface $other): bool
     {
         if (get_class($this) !== get_class($other)) {
-            throw new \InvalidArgumentException(sprintf('A Value Object of type %s can not be compared to another of type %s', get_class($this), get_class($other)));
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'A Value Object of type %s can not be compared to another of type %s',
+                    get_class($this),
+                    get_class($other)
+                )
+            );
         }
+
         return $this == $other;
     }
 }
